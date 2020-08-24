81d5f5f2553491d95b4ce0ebd00145deb045e479
MeridjaNassim <ha_meridja@esi.dz>
2020-08-24 23:45:34 +0100

    Removed theme switch

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -5,7 +5,7 @@
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-import React, { useState, useEffect } from "react"
+import React from "react"
 import Header from "./header"
 import Footer from "./footer"
 import "../../styles/layout.css"
@@ -14,17 +14,17 @@ import styled from "styled-components"
 import content from "../../content/layout.json"
 
 const Layout = ({ children }) => {
-  const [theme, setTheme] = useState("light")
-  const [animate, setAnimate] = useState(false)
-  useEffect(() => {
-    let storedTheme = localStorage.getItem("theme") || "light"
-    setTheme(storedTheme)
-  }, [])
+  // const [theme, setTheme] = useState("light")
+  // const [animate, setAnimate] = useState(false)
+  // useEffect(() => {
+  //   let storedTheme = localStorage.getItem("theme") || "light"
+  //   setTheme(storedTheme)
+  // }, [])
   return (
-    <div className={`${theme}-theme`}>
+    <div className={`light-theme`}>
       <Header />
       <div style={{ backgroundColor: "inherit" }}>
-        <ThemeSwitch
+        {/* <ThemeSwitch
           title={theme === "light" ? "switch to dark" : "switch to light"}
           onClick={e => {
             let newdata = theme === "light" ? "dark" : "light"
@@ -49,7 +49,7 @@ const Layout = ({ children }) => {
               alt="moon"
             />
           )}
-        </ThemeSwitch>
+        </ThemeSwitch> */}
         <Main style={{ backgroundColor: "inherit", minHeight: "100vh" }}>
           <noscript
             style={{
@@ -89,68 +89,68 @@ section {
   }
 }
 `
-const ThemeSwitch = styled.button`
-  position: fixed;
-  top: 2.5vh;
-  right: 10px;
-  height: 50px;
-  width: 50px;
-  border: none;
-  z-index: 9999;
-  cursor: pointer;
-  transform: scale(0);
-  outline: none;
-  background-color: transparent;
-  animation: grow 1s ease forwards 1;
-  opacity: 0.5;
-  img {
-    margin: 0;
-  }
-  transition: opacity 1s ease;
-  @keyframes grow {
-    from {
-      transform: scale(0);
-    }
-    to {
-      transform: scale(1);
-    }
-  }
-  img.shrink-grow {
-    animation: shrinkgrow 0.5s ease-out forwards 1 !important;
+// const ThemeSwitch = styled.button`
+//   position: fixed;
+//   top: 2.5vh;
+//   right: 10px;
+//   height: 50px;
+//   width: 50px;
+//   border: none;
+//   z-index: 9999;
+//   cursor: pointer;
+//   transform: scale(0);
+//   outline: none;
+//   background-color: transparent;
+//   animation: grow 1s ease forwards 1;
+//   opacity: 0.5;
+//   img {
+//     margin: 0;
+//   }
+//   transition: opacity 1s ease;
+//   @keyframes grow {
+//     from {
+//       transform: scale(0);
+//     }
+//     to {
+//       transform: scale(1);
+//     }
+//   }
+//   img.shrink-grow {
+//     animation: shrinkgrow 0.5s ease-out forwards 1 !important;
 
-    @keyframes shrinkgrow {
-      from {
-        transform: scale(0);
-      }
-      to {
-        transform: scale(1);
-      }
-    }
-  }
-  &:hover {
-    opacity: 1;
-    img {
-      animation: rotate1 2s linear forwards infinite;
+//     @keyframes shrinkgrow {
+//       from {
+//         transform: scale(0);
+//       }
+//       to {
+//         transform: scale(1);
+//       }
+//     }
+//   }
+//   &:hover {
+//     opacity: 1;
+//     img {
+//       animation: rotate1 2s linear forwards infinite;
 
-      @keyframes rotate1 {
-        0% {
-          transform: rotateZ(0deg);
-        }
-        25% {
-          transform: rotateZ(30deg);
-        }
-        75% {
-          transform: rotateZ(-30deg);
-        }
-        100% {
-          transform: rotateZ(0deg);
-        }
-      }
-    }
-  }
-  @media screen and (max-width: 768px) {
-    top: 12vh;
-  }
-`
+//       @keyframes rotate1 {
+//         0% {
+//           transform: rotateZ(0deg);
+//         }
+//         25% {
+//           transform: rotateZ(30deg);
+//         }
+//         75% {
+//           transform: rotateZ(-30deg);
+//         }
+//         100% {
+//           transform: rotateZ(0deg);
+//         }
+//       }
+//     }
+//   }
+//   @media screen and (max-width: 768px) {
+//     top: 12vh;
+//   }
+// `
 
 export default Layout
