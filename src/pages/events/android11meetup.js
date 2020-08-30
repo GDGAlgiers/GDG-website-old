01df7e8d5ad1ed936b44822770e17c6cfdff2e42
MeridjaNassim <ha_meridja@esi.dz>
2020-08-30 17:46:28 +0100

    Created Android 11 meetup page

---
+++ src/pages/events/android11meetup.js
@@ -0,0 +1,14 @@
+import React from "react"
+import PageLoader from "../../components/common/loader"
+import SEO from "../../components/layout/seo"
+/// remove the loader when you implement this page
+export default function android11meetup() {
+  return (
+    <
+   
+    >
+      <SEO title="ANDROID11 MEETUP" />
+      <PageLoader  loadingText="We are planning the event , see you soon"></PageLoader>
+    </>
+  )
+}
