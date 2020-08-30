bb962088b4b8eac8d8c0cf1161906c7834964aa7
MeridjaNassim <ha_meridja@esi.dz>
2020-08-30 16:38:37 +0100

    Improved Mobile Event Showcase

--- src/components/index/events/EventMobileShowcase.js
+++ src/components/index/events/EventMobileShowcase.js
@@ -1,51 +1,76 @@
-import React from 'react'
-import styled from 'styled-components'
-import {Link} from 'gatsby'
-function EventMobileShowcase({event}) {
-    return (
-        <StyledWrapper>
-            <EventLogo src={require(`../../../images/events/${event.logo}`)} alt={event.title}></EventLogo>
-            <Link to={event.url}>See details 🠖</Link>
-        </StyledWrapper>
-    )
+import React, { useState } from "react"
+import styled from "styled-components"
+import { Link } from "gatsby"
+import Fade from "react-reveal/Fade"
+function EventMobileShowcase({ event }) {
+  const [showContent, setShowContent] = useState(false)
+  const toggleContent = () => setShowContent(!showContent)
+  return (
+    <StyledWrapper>
+      <EventLogo
+        src={require(`../../../images/events/${event.logo}`)}
+        alt={event.title}
+      ></EventLogo>
+      <EventDescription className={showContent && "show__content"}>{event.content}</EventDescription>
+
+      <button onClick={toggleContent}>See details</button>
+    </StyledWrapper>
+  )
 }
 
-
-/// styled components 
-
+/// styled components
 
 const StyledWrapper = styled.div`
-    display : flex;
-    justify-content : space-around;
-    align-items : center;
-    flex-direction : column;
-    margin :20px;
-    border-radius : 20px;
-    height : 400px;
-    box-shadow: 0 5px 6px -3px rgba(0, 0, 0, 0.1),
-    0 9px 12px 1px rgba(0, 0, 0, 0.14), 0 3px 16px 2px rgba(0, 0, 0, 0.12);
-    background-image : url(${props => props.bg});
-    padding: 10px;
-    transition : 280ms ease-in;
-    &:hover {
-        box-shadow: 0 5px 6px -3px rgba(0, 0, 0, 0.2),
-    0 9px 12px 1px rgba(0, 0, 0, 0.14), 0 3px 16px 2px rgba(0, 0, 0, 0.12);
+  position: relative;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  flex-direction: column;
+  margin: 20px;
+  min-height: 50vh;
+  border-radius: 20px;
+  box-shadow: 0px 3px 10px rgba(0, 0, 0, 0.1);
+  background-image: url(${props => props.bg});
+  padding: 10px;
+  transition: 280ms ease-in;
+
+  button {
+    text-decoration: none;
+    background-color: transparent;
+    font-family: var(--font-header);
+    font-weight: 400;
+    font-size: 1rem;
+    padding: 15px;
+    border: none;
+    box-shadow: 0px 3px 10px rgba(0, 0, 0, 0.2);
+    border-radius: 5px;
+    color: var(--green);
+    outline : none;
+    &:focus{
+        box-shadow: 0px 3px 10px rgba(0, 0, 0, 0.05);
+        outline : none;
+        border : none;
     }
-    a {
-        text-decoration : none;
-        font-family : var(--font);
-        font-weight : 200;
-        font-size : 1rem;
-        padding : 10px;
-        border : 1px solid var(--green);
-        border-radius : 5px;
-        color : var(--green)
-    }
+  }
 `
 const EventLogo = styled.img`
-    max-height :100%;
-    max-width :100%;
-    padding : auto;
-    border-radius : 20px;
+  max-height: 300px;
+  max-width: 300px;
+  margin: 0;
+`
+const EventDescription = styled.p`
+  margin-top: 20%;
+  padding: 0 18px;
+  max-height : 0px;
+  transition: 280ms ease-in;
+  overflow-y: hidden;
+  font-family: var(--font-header);
+  &::-webkit-scrollbar {
+    display: none;
+  }
+  &.show__content {
+    max-height: 400px;
+    overflow-y: scroll;
+  }
 `
 export default EventMobileShowcase
--- src/components/index/events/Events.js
+++ src/components/index/events/Events.js
@@ -94,6 +94,9 @@ const Wrapper = styled.section`
   padding: 0 4rem;
   .slider-show {
     margin-bottom : 64px;
+    .slick-list {
+      height : auto !important;
+    }
   }
   @media screen and (max-width: 768px) {
     padding: 0;
